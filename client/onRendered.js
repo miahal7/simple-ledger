8794d51f3cffda42f74d83e1d3d9568c3788dcb5
Jeremiah <miahal77@hotmail.com>
2015-08-01 16:04:37 -0500

    Add conditional for headroom (desktop)
    Only initializes headroom on navbar when the device is a phone or tablet.

--- client/onRendered.js
+++ client/onRendered.js
@@ -1,10 +1,12 @@
 Template.body.onRendered(function () {
   console.log("Body Rendered");
   Meteor.call('deleteFlaggedTransactions', false);
-  $("#page-header").headroom({
-    "offset": 0,
-    "tolerance": 35
-  });
+  if(Meteor.Device.isPhone() || Meteor.DeviceisTablet()){
+    $("#page-header").headroom({
+      "offset": 0,
+      "tolerance": 35
+    });
+  }
 });
 
 Template.vendor.onRendered(function () {
