d05a552f3cefd4f2333352f07b9023dbcfe5ce1a
miahal7 <miahal77@gmail.com>
2017-04-08 17:14:24 -0500

    Finish abstracting the ui components

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -2,10 +2,18 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 // Import to load these templates
-import '../../ui/layouts/app-body.js';
+import '/imports/ui/layouts/app-body.js';
+import '/imports/ui/pages/ledger/ledger.js';
 
 FlowRouter.route('/', {
     name: 'App.home',
+    action() {
+        BlazeLayout.render('App_body', { main: 'app_rootRedirector' });
+    },
+});
+
+FlowRouter.route('/transactions/:month', {
+    name: 'month.transactions',
     action() {
         BlazeLayout.render('App_body', { main: 'app_rootRedirector' });
     },
