f3173b55953715ece6635218c2364d4553f931fa
miahal7 <jlucio@keyenergy.com>
2017-09-29 15:23:04 -0500

    Remove console.log from server

--- imports/api/transactions/server/publications.js
+++ imports/api/transactions/server/publications.js
@@ -3,7 +3,6 @@ import { Transactions } from '../transactions.js';
 
 Meteor.publish("transactions", function (month) {
     const trans = Transactions.find({ userId: this.userId, month });
-    console.log('TRANSACTIONS', trans.count());
 
     return trans;
 });
